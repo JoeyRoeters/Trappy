571b355a389fb3f30c8ecc0c918b2f2d235176a7
Jurrie <jurrie39@gmail.com>
2022-06-16 21:20:42 +0200

    GRO-29 - Removed logging

--- app/Listeners/SendTrapCatchNotification.php
+++ app/Listeners/SendTrapCatchNotification.php
@@ -31,7 +31,6 @@ class SendTrapCatchNotification implements ShouldQueue
         $trap = $event->trap;
         $time = $event->time;
 
-        \Illuminate\Support\Facades\Log::info($trap->id);
         $emailUsers = User::where('notification_settings->notify_email', true)
             ->whereJsonContains('notification_settings->traps', $trap->id)
             ->orWhere([['notification_settings->notify_email', true], ['notification_settings->traps', '=', '[]']])
